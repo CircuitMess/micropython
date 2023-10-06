b9511e72aae007d169adb383a351312fbc773bac
Filip Budiša <filip@circuitmess.com>
2023-10-06 11:04:43 +0200

    esp32/libraries/CircuitOS/RGBSolid: add const, rename set_rgb to set_color, push_color to push, add init to RGBSolidGPIO

--- ports/esp32/libraries/CircuitOS/_RGBSolid.py
+++ ports/esp32/libraries/CircuitOS/_RGBSolid.py
@@ -15,14 +15,14 @@ class RGBSolid:
         White = const(7)
 
     rgbMap = [
-        0b000,
-        0b100,
-        0b010,
-        0b001,
-        0b110,
-        0b101,
-        0b011,
-        0b111
+        const(0b000),
+        const(0b100),
+        const(0b010),
+        const(0b001),
+        const(0b110),
+        const(0b101),
+        const(0b011),
+        const(0b111)
     ]
 
     def __init__(self, pin_r: int, pin_g: int, pin_b: int):
@@ -31,14 +31,14 @@ class RGBSolid:
         self.pin_g = pin_g
         self.pin_b = pin_b
 
-    def set_rgb(self, color: int):
+    def set_color(self, color: int):
         self.color = color
-        self.push_color()
+        self.push()
 
-    def get_rgb(self) -> int:
+    def get_color(self) -> int:
         return self.color
 
-    def push_color(self):
+    def push(self):
         pass
 
 
@@ -49,8 +49,9 @@ class RGBSolidGPIO(RGBSolid):
         self.pin_r = Signal(Pin(pin_r, mode=Pin.OUT, value=False))
         self.pin_g = Signal(Pin(pin_g, mode=Pin.OUT, value=False))
         self.pin_b = Signal(Pin(pin_b, mode=Pin.OUT, value=False))
+        self.set_color(self.Color.Off)
 
-    def push_color(self):
+    def push(self):
         color_code = self.rgbMap[self.color]
         self.pin_r.on() if color_code & 0b100 else self.pin_r.off()
         self.pin_g.on() if color_code & 0b010 else self.pin_g.off()
@@ -65,9 +66,9 @@ class RGBSolidExpander(RGBSolid):
         self.expander.pin_mode(pin_r, Pin.OUT)
         self.expander.pin_mode(pin_g, Pin.OUT)
         self.expander.pin_mode(pin_b, Pin.OUT)
-        self.set_rgb(self.Color.Off)
+        self.set_color(self.Color.Off)
 
-    def push_color(self):
+    def push(self):
         color_code = self.rgbMap[self.color]
         self.expander.pin_write(self.pin_r, bool(color_code & 0b100))
         self.expander.pin_write(self.pin_g, bool(color_code & 0b010))
