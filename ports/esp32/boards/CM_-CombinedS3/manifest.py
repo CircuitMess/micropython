e448523e9a09a1511c5a7138a4f41e2df7d10161
Filip Budiša <filip@circuitmess.com>
2024-03-13 17:03:28 +0100

    ports/esp32: Add CombinedS3 board

---
+++ ports/esp32/boards/CM_-CombinedS3/manifest.py
@@ -0,0 +1,6 @@
+include("$(PORT_DIR)/boards/manifest.py")
+
+package("Bit", base_path="$(PORT_DIR)/libraries")
+package("Artemis", base_path="$(PORT_DIR)/libraries")
+package("MissionControl", base_path="$(PORT_DIR)/libraries")
+package("PerseRover", base_path="$(PORT_DIR)/libraries")
