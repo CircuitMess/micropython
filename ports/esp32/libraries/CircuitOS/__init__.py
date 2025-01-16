f90f33de138c0690dd32ed631b8ea175793fade6
Emil Gajšak <emil.gajsak@gmail.com>
2025-01-16 13:44:46 +0100

    lib/CircuitOS: ADS1015 external ADC support

--- ports/esp32/libraries/CircuitOS/__init__.py
+++ ports/esp32/libraries/CircuitOS/__init__.py
@@ -15,5 +15,6 @@ from .Devices.LIS2DW12 import *
 from .Devices.AW9523 import *
 from .Devices.LSM6DS3TR import *
 from .Devices.BM8563 import BM8563
+from .Devices.ADS1015 import *
 
 from ._Matrix import *
