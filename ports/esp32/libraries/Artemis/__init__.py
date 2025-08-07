d5e2b68ceffaff62b90a9490af8119c56c622d17
Emil Gajšak <emil.gajsak@gmail.com>
2025-08-07 14:06:18 +0200

    esp32/Artemis: Added support for rev2

--- ports/esp32/libraries/Artemis/__init__.py
+++ ports/esp32/libraries/Artemis/__init__.py
@@ -4,39 +4,47 @@ from CircuitOS import InputGPIO, Display, Piezo, RGB_LED, PanelST7735_128x128
 from .Pins import *
 from CircuitOS import LSM6DS3TR
 from CircuitOS import BM8563
+import efuse
 
-spiTFT: SPI = SPI(1, baudrate=27000000, polarity=0, phase=0, sck=Pin(Pins.TFT_SCK), mosi=Pin(Pins.TFT_MOSI))
+revision = efuse.read_rev()
 
-backlight = Signal(Pin(Pins.BL, mode=Pin.OUT, value=True), invert=True)
+pins = Pins(revision)
+btn_pins = Buttons(pins)
 
-buttons = InputGPIO(Buttons.Pins, inverted=False)
+spiTFT: SPI = SPI(1, baudrate=27000000, polarity=0, phase=0, sck=Pin(pins.get(Pins.TFT_SCK)),
+				  mosi=Pin(pins.get(Pins.TFT_MOSI)))
 
-piezo = Piezo(Pins.BUZZ)
-rgb = RGB_LED((Pins.RGB_R, Pins.RGB_G, Pins.RGB_B), True)
-panel = PanelST7735_128x128(spiTFT, dc=Pin(Pins.TFT_DC, Pin.OUT), reset=Pin(Pins.TFT_RST, Pin.OUT), rotation=1)
+backlight = Signal(Pin(pins.get(Pins.BL), mode=Pin.OUT, value=True), invert=True)
+
+buttons = InputGPIO(btn_pins.get_pins_array(), inverted=False)
+
+piezo = Piezo(pins.get(Pins.BUZZ))
+rgb = RGB_LED((pins.get(Pins.RGB_R), pins.get(Pins.RGB_G), pins.get(Pins.RGB_B)), True)
+panel = PanelST7735_128x128(spiTFT, dc=Pin(pins.get(Pins.TFT_DC), Pin.OUT), reset=Pin(pins.get(Pins.TFT_RST), Pin.OUT),
+							rotation=3 if revision == 2 else 1)
 display = Display(panel)
 
-i2c = I2C(0, sda=Pin(Pins.I2C_SDA), scl=Pin(Pins.I2C_SCL))
+i2c = I2C(0, sda=Pin(pins.get(Pins.I2C_SDA)), scl=Pin(pins.get(Pins.I2C_SCL)))
 
 imu = LSM6DS3TR(i2c)
 rtc = BM8563(i2c)
 
+leds = [Signal(Pin(pins.get(Pins.LED_1), mode=Pin.OUT, value=False), invert=False),
+		Signal(Pin(pins.get(Pins.LED_2), mode=Pin.OUT, value=False), invert=False),
+		Signal(Pin(pins.get(Pins.LED_3), mode=Pin.OUT, value=False), invert=False),
+		Signal(Pin(pins.get(Pins.LED_4), mode=Pin.OUT, value=False), invert=False),
+		Signal(Pin(pins.get(Pins.LED_5), mode=Pin.OUT, value=False), invert=False),
+		Signal(Pin(pins.get(Pins.LED_6), mode=Pin.OUT, value=False), invert=False)]
 
-leds = [Signal(Pin(Pins.LED_1, mode=Pin.OUT, value=False), invert=False),
-        Signal(Pin(Pins.LED_2, mode=Pin.OUT, value=False), invert=False),
-        Signal(Pin(Pins.LED_3, mode=Pin.OUT, value=False), invert=False),
-        Signal(Pin(Pins.LED_4, mode=Pin.OUT, value=False), invert=False),
-        Signal(Pin(Pins.LED_5, mode=Pin.OUT, value=False), invert=False),
-        Signal(Pin(Pins.LED_6, mode=Pin.OUT, value=False), invert=False)]
 
 def begin():
-    imu.begin()
-    rtc.begin()
-    panel.init()
+	imu.begin()
+	rtc.begin()
+	panel.init()
 
-    display.fill(Display.Color.Black)
-    display.commit()
+	display.fill(Display.Color.Black)
+	display.commit()
 
-    backlight.on()
+	backlight.on()
 
-    buttons.scan()
+	buttons.scan()
