5d1fd24bdfab85b8d50f1d1f1c99b8ecef832744
Emil Gajšak <emil.gajsak@gmail.com>
2025-08-08 10:39:07 +0200

    esp32/Chatter: Added support for rev2

--- ports/esp32/libraries/Chatter/__init__.py
+++ ports/esp32/libraries/Chatter/__init__.py
@@ -2,12 +2,22 @@ from ST7735 import TFT
 from machine import SPI, Pin, Signal
 from CircuitOS import InputShift, Piezo, Display, PanelST7735
 from .Pins import *
+import efuse
 
+revision = efuse.read_rev()
 
-spiLora: SPI = SPI(1, baudrate=27000000, polarity=0, phase=0, sck=Pin(Pins.LORA_SCK), mosi=Pin(Pins.LORA_MOSI), miso=Pin(Pins.LORA_MISO))
+spiLora: SPI = SPI(1, baudrate=27000000, polarity=0, phase=0, sck=Pin(Pins.LORA_SCK), mosi=Pin(Pins.LORA_MOSI),
+				   miso=Pin(Pins.LORA_MISO))
 
 spiTFT: SPI = SPI(2, baudrate=27000000, polarity=0, phase=0, sck=Pin(Pins.TFT_SCK), mosi=Pin(Pins.TFT_MOSI))
-panel = PanelST7735(spiTFT, dc=Pin(Pins.TFT_DC, Pin.OUT), reset=Pin(Pins.TFT_RST, Pin.OUT), cs=Pin(Pins.TFT_CS, Pin.OUT), rotation=1)
+
+if (revision == 1):
+	panel = PanelST7735(spiTFT, dc=Pin(Pins.TFT_DC, Pin.OUT), reset=Pin(13, Pin.OUT),
+						cs=Pin(15, Pin.OUT), rotation=1, color_order_rgb=True)
+else:
+	panel = PanelST7735(spiTFT, dc=Pin(Pins.TFT_DC, Pin.OUT), reset=Pin(Pins.TFT_RST, Pin.OUT),
+						cs=Pin(Pins.TFT_CS, Pin.OUT), rotation=1)
+
 display = Display(panel)
 blPin = Pin(Pins.BL, mode=Pin.OUT, value=True)
 backlight = Signal(blPin, invert=True)
@@ -19,6 +29,11 @@ piezo = Piezo(Pins.BUZZ)
 
 def begin():
 	panel.init()
+
+	# Can't set TFT offset before this, since panel.init() resets it to default
+	if (revision == 1):
+		panel.tft.offset(1, 2)
+
 	display.fill(display.Color.Black)
 	display.commit()
 
