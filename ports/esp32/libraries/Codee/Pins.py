dc10c9f78e8ab36e17c79e24d4a2a4bc4db45cc2
Emil Gajšak <emil.gajsak@gmail.com>
2025-08-08 16:14:37 +0200

    esp32/Codee: Added support for rev2

--- ports/esp32/libraries/Codee/Pins.py
+++ ports/esp32/libraries/Codee/Pins.py
@@ -2,20 +2,75 @@ from micropython import const
 
 
 class Pins:
-	BL: int = const(45)
-	BATT: int = const(6)
+	BL: int = const(0)
+	BATT: int = const(1)
 
-	TFT_SCK: int = const(39)
-	TFT_MOSI: int = const(40)
-	TFT_DC: int = const(41)
-	TFT_RST: int = const(42)
+	TFT_SCK: int = const(2)
+	TFT_MOSI: int = const(3)
+	TFT_DC: int = const(4)
+	TFT_RST: int = const(5)
 
-	BUZZ: int = const(44)
+	BUZZ: int = const(6)
 
-	I2C_SDA: int = const(33)
-	I2C_SCL: int = const(34)
+	I2C_SDA: int = const(7)
+	I2C_SCL: int = const(8)
 
-	LED: int = const(38)
+	LED: int = const(9)
+
+	BtnA: int = const(10)
+	BtnB: int = const(11)
+	BtnC: int = const(12)
+	BtnD: int = const(13)
+
+	Rev1Map = {
+		BL: const(45),
+		BATT: const(6),
+		TFT_SCK: const(39),
+		TFT_MOSI: const(40),
+		TFT_DC: const(41),
+		TFT_RST: const(42),
+		BUZZ: const(44),
+		I2C_SDA: const(33),
+		I2C_SCL: const(34),
+		LED: const(38),
+		BtnA: const(2),
+		BtnB: const(3),
+		BtnC: const(4),
+		BtnD: const(5)
+	}
+
+	Rev2Map = {
+		BL: const(6),
+		BATT: const(1),
+		TFT_SCK: const(2),
+		TFT_MOSI: const(3),
+		TFT_DC: const(4),
+		TFT_RST: const(5),
+		BUZZ: const(7),
+		I2C_SDA: const(36),
+		I2C_SCL: const(35),
+		LED: const(10),
+		BtnA: const(16),
+		BtnB: const(17),
+		BtnC: const(9),
+		BtnD: const(8)
+	}
+
+	def __init__(self, revision):
+		self.currentMap = None
+		if revision == 0:
+			self.currentMap = self.Rev1Map
+		elif revision == 1:
+			self.currentMap = self.Rev2Map
+		else:
+			print("Unknown revision", revision)
+
+	def get(self, pin: int) -> int:
+		if not pin in self.currentMap:
+			print("Pin", pin, "not in map")
+			return -1
+
+		return self.currentMap[pin]
 
 
 class Buttons:
@@ -24,10 +79,14 @@ class Buttons:
 	C: int = const(2)
 	D: int = const(3)
 
-	# Maps Buttons [0-3] to their respective GPIO pins
-	Pins: [int] = const([
-		const(2),
-		const(3),
-		const(4),
-		const(5)
-	])
+	def __init__(self, pins: Pins):
+		# Maps Buttons [0-3] to their respective GPIO pins
+		self.Pins: [int] = const([
+			pins.get(Pins.BtnA),
+			pins.get(Pins.BtnB),
+			pins.get(Pins.BtnC),
+			pins.get(Pins.BtnD)
+		])
+
+	def get_pins_array(self) -> [int]:
+		return self.Pins
--- ports/esp32/libraries/Codee/__init__.py
+++ ports/esp32/libraries/Codee/__init__.py
@@ -1,23 +1,34 @@
 from machine import SPI, Pin, Signal, I2C
 from CircuitOS import InputGPIO, Display, Piezo, PanelST7735_128x128, BM8563
 from .Pins import *
+import efuse
 
-spiTFT: SPI = SPI(1, baudrate=27000000, polarity=0, phase=0, sck=Pin(Pins.TFT_SCK), mosi=Pin(Pins.TFT_MOSI))
+revision = efuse.read_rev()
 
-blPin = Pin(Pins.BL, mode=Pin.OUT, value=True)
+pins = Pins(revision)
+btn_pins = Buttons(pins)
+
+spiTFT: SPI = SPI(1, baudrate=27000000, polarity=0, phase=0, sck=Pin(pins.get(Pins.TFT_SCK)),
+				  mosi=Pin(pins.get(Pins.TFT_MOSI)))
+
+blPin = Pin(pins.get(Pins.BL), mode=Pin.OUT, value=True)
 backlight = Signal(blPin, invert=True)
 
-buttons = InputGPIO(Buttons.Pins, inverted=True)
+buttons = InputGPIO(btn_pins.get_pins_array(), inverted=True)
 
-piezo = Piezo(Pins.BUZZ)
-dc = Pin(Pins.TFT_DC, Pin.OUT)
-reset = Pin(Pins.TFT_RST, Pin.OUT)
-panel = PanelST7735_128x128(spiTFT, dc=dc, reset=reset, rotation=3)
+piezo = Piezo(pins.get(Pins.BUZZ))
+dc = Pin(pins.get(Pins.TFT_DC), Pin.OUT)
+reset = Pin(pins.get(Pins.TFT_RST), Pin.OUT)
+
+if (revision == 1):
+	panel = PanelST7735_128x128(spiTFT, dc=dc, reset=reset, rotation=1)
+else:
+	panel = PanelST7735_128x128(spiTFT, dc=dc, reset=reset, rotation=3)
 display = Display(panel)
 
-i2c = I2C(0, sda=Pin(Pins.I2C_SDA), scl=Pin(Pins.I2C_SCL))
+i2c = I2C(0, sda=Pin(pins.get(Pins.I2C_SDA)), scl=Pin(pins.get(Pins.I2C_SCL)))
 rtc = BM8563(i2c)
-led = Signal(Pin(Pins.LED, mode=Pin.OUT, value=False), invert=False)
+led = Signal(Pin(pins.get(Pins.LED), mode=Pin.OUT, value=False), invert=False)
 
 
 def begin():
